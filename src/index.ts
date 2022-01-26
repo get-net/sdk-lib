64faeb932b51ce9d515bae5eceb0302b53f7ff6d
Олег Гладилов <og@get-net.ru>
2022-01-26 16:25:22 +0500

    test on client

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
 import EncodePKCE from './utils/utils';
 
+interface IobjectData {
+  def_key: string;
+  sha256_key: string;
+}
+
 class CustomSdk {
-  protected url: string;
-  protected clientId: string;
-  protected redirectUrl: string;
+  // protected url: string;
+  // protected clientId: string;
+  // protected redirectUrl: string;
+  //
+  // constructor(url: string, clientId: string, redirectUrl: string) {
+  //   this.url = url;
+  //   this.clientId = clientId;
+  //   this.redirectUrl = redirectUrl;
+  // }
 
-  constructor(url: string, clientId: string, redirectUrl: string) {
-    this.url = url;
-    this.clientId = clientId;
-    this.redirectUrl = redirectUrl;
+  private create_code_verifier(): Promise<IobjectData> {
+    const helper = new EncodePKCE();
+    const num:number = Math.floor(Math.random() * 127) + 43
+    return helper.getCodes(num);
   }
-
-  private create_code_verifier(): string {
-    const sdkMagic = new EncodePKCE();
-    const codes = sdkMagic.getCodes(42);
-    return '1';
-  }
-  public test(): number {
-    this.create_code_verifier();
-    return 42;
+  //testing
+  public xxx(): void {
+    this.create_code_verifier()
+        .then((data) => {
+          if(data.def_key && data.sha256_key) {
+            localStorage.setItem("pkce_code_verifier", data.def_key);
+            return data
+          }
+        })
   }
 }
 
